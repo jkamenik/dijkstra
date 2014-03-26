76bba26bef3410150c5d4d165b05107504f2585c
John Kamenik <jkamenik@gmail.com>
2014-03-25 22:15:44 -0400

    testing node helpers

--- lib/djikstra/node.rb
+++ lib/djikstra/node.rb
@@ -1,15 +1,38 @@
 module Djikstra
   class Node
-    attr_accessor :name, :distance, :neighbors
+    attr_accessor :name, :distance, :neighbors, :previous_node
     
     def initialize(name)
       @name      = name
-      @distance  = :infinity
       @neighbors = []
+      @weights   = {}
+      reset
     end
     
-    def neighbor_of(node)
-      @neighbors.push node
+    def reset
+      @distance      = :infinity
+      @previous_node = nil
     end
+    
+    def neighbor_of(node, weight)
+      @neighbors.push node
+      @weights[node.name] = weight
+    end
+    
+    def distance_to(node)
+      @weights[node.name]
+    end
+    
+    def >(number)
+      return true if @distance == :infinity
+      return false if number == :infinity
+      
+      distance > number
+    end
+    
+    def inspect
+      %Q(#<Djikstra::Node name="#{name}" distance=#{distance}>)
+    end
+    alias_method :to_s, :inspect
   end
 end
---
+++ spec/djikstra/node_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe Djikstra::Node do
+  let(:node) { Djikstra::Node.new 'A' }
+  
+  context '.reset' do
+    it 'resets distance to infinity' do
+      node.distance = 1
+      
+      node.reset
+      expect(node.distance).to eq :infinity
+    end
+    
+    it 'removes any previous nodes' do
+      node.previous_node = node
+      
+      node.reset
+      expect(node.reset).to eq nil
+    end
+  end
+  
+  context ">" do
+    it 'returns true if my distance is infinity' do
+      node.distance = :infinity
+      
+      expect(node > 1_000_000_000_000).to be_true
+    end
+    
+    it 'returns false if the number is infinity' do
+      node.distance = 1_000_000_000_000
+      
+      expect(node > :infinity).to be_false
+    end
+    
+    it 'compares distance to number' do
+      node.distance = 1
+      
+      expect(node > 2).to be_false
+    end
+  end
+end
